2a600712263d6e301d61c1bda5082c1fca29f326
TJMoore <mooreth4791@gmail.com>
2024-03-05 08:59:59 -0500

    got routes working not the way the tutorial said

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'heroes',
+        component: HeroesComponent,
+        title: 'Heroes'
+    }
+
+];
